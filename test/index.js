b13f37d2588b40dc68b84938034feeca2e404510
Keita Akutsu <kakts.git@gmail.com>
2021-04-08 00:42:15 +0900

    test: fix mutation.setUserNameRandomly tests

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@
 
 let setupCompleted = false
 const app = require('../lib/index')
+const logger = require('../lib/log/logger').console
 
 const axios = require('axios')
 const testApiClient = axios.create({
@@ -15,14 +16,14 @@ const testApiClient = axios.create({
  * テスト用サーバのセットアップ
  */
 function setup() {
-    console.log(`test setting up. ${setupCompleted}`)
+    logger.log(`test setting up. ${setupCompleted}`)
     if (setupCompleted) {
         // setupが完了している場合はスキップ
-        console.debug("test/index.js setting up has already been completed. skip setting up.")
+        logger.debug("test/index.js setting up has already been completed. skip setting up.")
         return
     }
 
-    console.log("[test.index] setting up graphql server for test.")
+    logger.log("[test.index] setting up graphql server for test.")
     app.startServer()
 
     setupCompleted = true;
